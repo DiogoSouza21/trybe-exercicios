bc1e43e5187e774edad95c029ec2824fe78326fd
Diogo Rosa de Souza <diogo21souza@outlook.com.br>
2022-12-29 23:23:00 -0300

    Delete Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-1-Node.js:Runtime-Assíncrono/Exercicio-1 directory
    Deletando diretório Exercicio-1

--- Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-1-Node.js:Runtime-Assíncrono/Exercicio-1/src/bmi.js
+++
@@ -1,6 +0,0 @@
-const readline = require('readline-sync');
-
-const pesoI = () => readline.question('Insira o peso em kilograma: ');
-const alturaI = () => readline.question('Insira a altura em metros: ');
-
-module.exports = { pesoI, alturaI };
