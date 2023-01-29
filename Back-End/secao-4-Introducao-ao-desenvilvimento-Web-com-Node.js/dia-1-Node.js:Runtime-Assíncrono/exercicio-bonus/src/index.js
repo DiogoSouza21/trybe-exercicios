5ca3f25924abfc2b8a513c3bc62eb68f9cb9de60
DiogoSouza21 <diogo21souza.com.br>
2023-01-29 20:40:01 -0300

    feat: Exercicio bonus 2 - Promise

--- Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-1-Node.js:Runtime-Assíncrono/exercicio-bonus/src/index.js
+++ Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-1-Node.js:Runtime-Assíncrono/exercicio-bonus/src/index.js
@@ -1,12 +1,25 @@
 const readLine = require('readline-sync');
 const fibonacci = require('./fibonacci');
+const promise = require('./promise'); 
+
+// function fibo {
+//   const numFibo = readLine.questionInt('Até qual nº de Fibonacci: ');
+//   if(numFibo >= 0){
+//     const arrFibo = fibonacci(numFibo);
+//     console.log(arrFibo);
+//   } else console.log("Número inválido!!!");
+// }
+
+function nPromise() {
+  const num1 = readLine.questionInt('Primeiro número: ');
+  const num2 = readLine.questionInt('Segundo número: ');
+  const num3 = readLine.questionInt('Terceiro número: ');
+  promise(num1, num2, num3);
+}
 
 function main() {
-  const numFibo = readLine.questionInt('Até qual nº de Fibonacci: ');
-  if(numFibo >= 0){
-    const arrFibo = fibonacci(numFibo);
-    console.log(arrFibo);
-  } else console.log("Número inválido!!!");
+  // fibo();
+  nPromise();
 }
 
 main();
