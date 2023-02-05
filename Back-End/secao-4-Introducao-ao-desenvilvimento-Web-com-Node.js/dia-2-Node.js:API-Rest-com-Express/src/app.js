ab537377a3af17aaf78330ef903d4439a3265de5
DiogoSouza21 <diogo21souza.com.br>
2023-02-04 23:29:12 -0300

    Criando o endpoint GET da rota /movies/:id

--- Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-2-Node.js:API-Rest-com-Express/src/app.js
+++ Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-2-Node.js:API-Rest-com-Express/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
-const readFile = require('./helpers/fs/readFile');
+const readFile = require('./helpers/fs/readFile.js');
+const movieRouter = require('./routes/moviesRoutes.js');
 
 const app = express();
 
 app.use(express.json());
+app.use("/movies", movieRouter);
 
 module.exports = app;
