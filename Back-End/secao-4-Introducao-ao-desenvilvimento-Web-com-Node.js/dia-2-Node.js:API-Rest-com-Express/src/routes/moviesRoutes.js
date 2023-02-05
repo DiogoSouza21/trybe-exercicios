574c4cdab185f73bb675de6485a29b58bea6938c
DiogoSouza21 <diogo21souza.com.br>
2023-02-04 23:56:56 -0300

    Criando o endpoint POST da rota /movies e cadastrando filme

--- Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-2-Node.js:API-Rest-com-Express/src/routes/moviesRoutes.js
+++ Back-End/secao-4-Introducao-ao-desenvilvimento-Web-com-Node.js/dia-2-Node.js:API-Rest-com-Express/src/routes/moviesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const movieRouter = express.Router();
-const readFile = require('../helpers/fs/readFile.js');
+const readFile = require('../helpers/fs/readFile');
+const writeFile = require('../helpers/fs/writeFile')
 
 movieRouter.get('/:id', async (req, res) => {
   try {
@@ -9,6 +10,32 @@ movieRouter.get('/:id', async (req, res) => {
     const movieSearch = movies.find((movie) => movie.id === Number(id));
     if(!movieSearch) return res.status(404).json({ message: 'Movie not found'});
     return res.status(200).json(movieSearch);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+movieRouter.get('/', async (req, res) => {
+  try {
+    const movies = await readFile();
+    return res.status(200).json(movies);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+movieRouter.post('/', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await writeFile(allMovies);
+    return res.status(200).json(allMovies);
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
